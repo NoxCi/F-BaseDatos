d7ce5a5ee7730b888f9f9a47d4ee785a1772ff66
NoxCi <noxorum@ciencias.unam.mx>
2017-10-12 19:14:54 -0500

    Practica07 v1.0

--- Practica07/eliminacion.sql
+++ Practica07/eliminacion.sql
@@ -1,5 +1,5 @@
-delete from geografico_normailzado.estados where not id_estado = 1;
-delete from geografico_normailzado.distritos_federales where not id_estado = 1;
-delete from geografico_normailzado.distritos_locales where not id_estado = 1;
-delete * from geografico_normailzado.municipios where not id_estado = 1;
-delete * from geografico_normailzado.secciones where not id_estado = 1;
+delete from geografico_normalizado.secciones where not id_estado = 1;
+delete from geografico_normalizado.distritos_federales where not id_estado = 1;
+delete from geografico_normalizado.distritos_locales where not id_estado = 1;
+delete from geografico_normalizado.municipios where not id_estado = 1;
+delete from geografico_normalizado.estados where not id_estado = 1;
