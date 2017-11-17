417414441f4019460a81102e56d7c1aa650ec280
NoxCi <noxorum@ciencias.unam.mx>
2017-11-16 19:24:28 -0600

    ...

--- Practica11/restriccionesBasicas.sql
+++ Practica11/restriccionesBasicas.sql
@@ -1,92 +1,155 @@
-set search_path to casillas, geografico, partidos_politicos, representantes, public;
---Resticciones y comentarios de Casillas
---casillas
-create domain BCSE as char check(value in ('B','C','S','E'));
-create domain SN as char check(value in ('S','N'));
-alter table casillas drop tipo_casilla;
-alter table casillas add tipo_casilla BCSE;
-alter table casillas drop aprobada;
-alter table casillas add aprobada SN;
-alter table casillas add constraint
-  PKCasillas primary key (id_estado, id_distrito_federal, seccion, tipo_casilla);
-alter table casillas add constraint
-  FKDistritosFederales foreign key (id_estado, id_distrito_federal)
-                       references distritos_federales (id_estado, id_distrito_federal);
-comment on table casillas is 'Tabla que contiene a las casillas por secciones.';
- comment on column casillas.id_estado is 'Identificador del estado a la que pertenece la casilla.';
- comment on column casillas.id_distrito_federal is 'Identificador del distrito federal a la que pertenenece la casilla.';
- comment on column casillas.seccion is 'La seccion a la cual pertenece la casilla.';
- comment on column casillas.tipo_casilla is 'Hace referencia al tipo que puede ser una casilla y solo puede ser B, C, S, E.';
- comment on column casillas.aprobada is 'Hace referencia a si la casilla esta aprobada o no, teniendo como dominio S o N.';
---casillas
-
+SET search_path to casillas, geografico, partidos_politicos, representantes, public;
 --Restricciones y comentarios de Geografico
 --estados
-alter table estados add constraint
-  PKEstados primary key (id_estado);
-comment on table estados is 'Tabla con los estados de la Republica Mexicana';
-  comment on table id_estado is 'Identificador de los estados.';
-  comment on table nombre is 'Nombre de los respectivos estado.';
-  comment on table abreviatura is 'Abreviatura del nombre de los estados.';
+ALTER TABLE estados ADD CONSTRAINT
+  PKEstados PRIMARY KEY (id_estado);
+  --ON UPDATE cascade
+  --ON DELETE casdade;
+COMMENT ON TABLE estados IS 'Tabla con los estados de la Republica Mexicana';
+  COMMENT ON COLUMN estados.id_estado IS 'Identificador de los estados.';
+  COMMENT ON COLUMN estados.nombre IS 'Nombre de los respectivos estado.';
+  COMMENT ON COLUMN estados.abreviatura IS 'Abreviatura del nombre de los estados.';
 --estados
 
 --municipios
-alter table municipios add constraint
-  PKMunicipios primary key (id_estado, id_municipio);
-alter table municipios add constraint
-  FKEstadosM foreign key (id_estado)
-            references estados (id_estado);
-comment on table municipios is 'Tabla con los municipios respectivos de cada estado.';
-  comment on column municipios.id_estado is 'Identificador del estado al cual pertenece el municipio.';
-  comment on column municipios.id_municipio is 'Identificador del municipio.';
-  comment on column municipios.nombre_municipio is 'Nombre del municipio.';
+ALTER TABLE municipios ADD CONSTRAINT
+  PKMunicipios PRIMARY KEY (id_estado, id_municipio);
+  --ON UPDATE cascade
+  --ON DELETE casdade;
+ALTER TABLE municipios ADD CONSTRAINT
+  FKEstadosM FOREIGN KEY (id_estado)
+            REFERENCES estados (id_estado)  match full;
+COMMENT ON TABLE municipios IS 'Tabla con los municipios respectivos de cada estado.';
+  COMMENT ON COLUMN municipios.id_estado IS 'Identificador del estado al cual pertenece el municipio.';
+  COMMENT ON COLUMN municipios.id_municipio IS 'Identificador del municipio.';
+  COMMENT ON COLUMN municipios.nombre_municipio IS 'Nombre del municipio.';
 --municipios
 
 --distritos_federales
-alter table distritos_federales add constraint
-  PKDistritosFederales primary key (id_estado, id_distrito_federal);
-alter table distritos_federales add constraint
-  FKEstadosDF foreign key (id_estado)
-            references estados (id_estado);
-comment on table distritos_federales is 'Tabla con los distritos federales.';
-  comment on column distritos_federales.id_estado is 'Identificador del estado del cual pertenece el distrito federal.';
-  comment on column distritos_federales.id_distrito_federal is 'Identificador del distrito federal.';
-  comment on column distritos_federales.cabecera_distrital_federal is 'Nombre del distrito federal.';
+ALTER TABLE distritos_federales ADD CONSTRAINT
+  PKDistritosFederales PRIMARY KEY (id_estado, id_distrito_federal);
+  --ON UPDATE cascade
+  --ON DELETE casdade;
+ALTER TABLE distritos_federales ADD CONSTRAINT
+  FKEstadosDF FOREIGN KEY (id_estado)
+            REFERENCES estados (id_estado)  match full;
+COMMENT ON TABLE distritos_federales IS 'Tabla con los distritos federales.';
+  COMMENT ON COLUMN distritos_federales.id_estado IS 'Identificador del estado del cual pertenece el distrito federal.';
+  COMMENT ON COLUMN distritos_federales.id_distrito_federal IS 'Identificador del distrito federal.';
+  COMMENT ON COLUMN distritos_federales.cabecera_distrital_federal IS 'Nombre del distrito federal.';
 --distritos_federales
 
 --distritos_locales
-alter table distritos_locales add constraint
-  PKDistritosLocales primary key (id_estado, id_distrito_federal);
-alter table distritos_locales add constraint
-  FKEstadosDL foreign key (id_estado)
-            references estados (id_estado);
-comment on table distritos_locales is 'Tabla con los distritos locales.';
-  comment on column distritos_locales.id_estado is 'Identificador del estado del cual pertenece el distrito local.';
-  comment on column distritos_locales.id_distrito_local is 'Identificador del distrito local.';
-  comment on column distritos_locales.cabecera_distrital_local is 'Nombre del distrito local.';
+ALTER TABLE distritos_locales ADD CONSTRAINT
+  PKDistritosLocales PRIMARY KEY (id_estado, id_distrito_local);
+  --ON UPDATE cascade
+  --ON DELETE casdade;
+ALTER TABLE distritos_locales ADD CONSTRAINT
+  FKEstadosDL FOREIGN KEY (id_estado)
+            REFERENCES estados (id_estado)  match full;
+COMMENT ON TABLE distritos_locales IS 'Tabla con los distritos locales.';
+  COMMENT ON COLUMN distritos_locales.id_estado IS 'Identificador del estado del cual pertenece el distrito local.';
+  COMMENT ON COLUMN distritos_locales.id_distrito_local IS 'Identificador del distrito local.';
+  COMMENT ON COLUMN distritos_locales.cabecera_distrital_local IS 'Nombre del distrito local.';
 --distritos_locales
 
 
 --secciones
-alter table secciones add constraint
-  PKSecciones primary key (id_estado, id_distrito_federal, id_distrito_local, id_municipio, seccion);
-alter table secciones add constraint
-  FKEstadosS foreign key (id_estado)
-            references estados (id_estado);
-alter table secciones add constraint
-  FKDistritosFederales foreign key (id_estado, id_distrito_federal)
-                       references distritos_federales (id_estado, id_distrito_federal);
-alter table secciones add constraint
- FKDistritosLoacales foreign key (id_estado, id_distrito_local)
-                      references distritos_locales (id_estado, id_distrito_local);
-alter table secciones add constraint
-  FKMunicipios foreign key (id_estado, id_municipios)
-               references municipios (id_estado, id_municipio);
-comment on table secciones is 'Tabla con todas las secciones de cada DF, DL y Municipio.';
-  comment on column secciones.id_estado is 'Identificador del estado.';
-  comment on column secciones.id_distrito_federal is 'Identificador del ditrito federal asociado a un estado.';
-  comment on column secciones.id_distrito_local is 'Identificador del distrito local asociado a un estado.';
-  comment on column secciones.id_municipio is 'Identificador del municipio asociado a un estado.';
-  comment on column secciones.seccion is 'Numero de la seccion asociado a un estado, ditrito fedral, ditrito local y municipio.';
-  comment on column secciones.tipo is 'El tipo de la seccion.';
+ALTER TABLE secciones ADD tipo char(1);
+ALTER TABLE secciones ADD CONSTRAINT
+  PKSecciones PRIMARY KEY (id_estado, id_distrito_federal, id_distrito_local, id_municipio, seccion);
+ALTER TABLE secciones ADD CONSTRAINT
+  FKEstadosS FOREIGN KEY (id_estado)
+            REFERENCES estados (id_estado) match full;
+ALTER TABLE secciones ADD CONSTRAINT
+  FKDistritosFederalesS FOREIGN KEY (id_estado, id_distrito_federal)
+                       REFERENCES distritos_federales (id_estado, id_distrito_federal) match full;
+ALTER TABLE secciones ADD CONSTRAINT
+ FKDistritosLoacalesS FOREIGN KEY (id_estado, id_distrito_local)
+                      REFERENCES distritos_locales (id_estado, id_distrito_local)  match full;
+ALTER TABLE secciones ADD CONSTRAINT
+  FKMunicipiosS FOREIGN KEY (id_estado, id_municipio)
+               REFERENCES municipios (id_estado, id_municipio)  match full;
+COMMENT ON TABLE secciones IS 'Tabla con todas las secciones de cada DF, DL y Municipio.';
+  COMMENT ON COLUMN secciones.id_estado IS 'Identificador del estado.';
+  COMMENT ON COLUMN secciones.id_distrito_federal IS 'Identificador del ditrito federal asociado a un estado.';
+  COMMENT ON COLUMN secciones.id_distrito_local IS 'Identificador del distrito local asociado a un estado.';
+  COMMENT ON COLUMN secciones.id_municipio IS 'Identificador del municipio asociado a un estado.';
+  COMMENT ON COLUMN secciones.seccion IS 'Numero de la seccion asociado a un estado, ditrito fedral, ditrito local y municipio.';
+  COMMENT ON COLUMN secciones.tipo IS 'El tipo de la seccion.';
 --secciones
+
+--Resticciones y comentarios de Casillas
+--casillas
+CREATE DOMAIN BCSE AS char CHECK(value IN ('B','C','S','E'));
+CREATE DOMAIN SN AS char CHECK(value IN ('S','N'));
+ALTER TABLE casillas DROP tipo_casilla;
+ALTER TABLE casillas ADD tipo_casilla BCSE;
+ALTER TABLE casillas DROP aprobada;
+ALTER TABLE casillas ADD aprobada SN;
+ALTER TABLE casillas ADD id_distrito_local integer;
+ALTER TABLE casillas ADD id_municipio integer;
+ALTER TABLE casillas ADD CONSTRAINT
+  PKCasillas PRIMARY KEY (id_estado, id_distrito_federal, seccion, tipo_casilla);
+ALTER TABLE casillas ADD CONSTRAINT
+ FKEstadosC FOREIGN KEY (id_estado)
+            REFERENCES estados (id_estado) match full;
+ALTER TABLE casillas ADD CONSTRAINT
+  FKDistritosFederalesC FOREIGN KEY (id_estado, id_distrito_federal)
+                       REFERENCES distritos_federales (id_estado, id_distrito_federal)  match full;
+ALTER TABLE casillas ADD CONSTRAINT
+  FKSeccionsC FOREIGN KEY (id_estado, id_distrito_federal, id_distrito_local, id_municipio, seccion)
+                     REFERENCES secciones (id_estado, id_distrito_federal, id_distrito_local, id_municipio, seccion) match full;
+COMMENT ON TABLE casillas IS 'Tabla que contiene a las casillas por secciones.';
+ COMMENT ON COLUMN casillas.id_estado IS 'Identificador del estado a la que pertenece la casilla.';
+ COMMENT ON COLUMN casillas.id_distrito_federal IS 'Identificador del distrito federal a la que pertenenece la casilla.';
+ COMMENT ON COLUMN casillas.seccion IS 'La seccion a la cual pertenece la casilla.';
+ COMMENT ON COLUMN casillas.tipo_casilla IS 'Hace referencia al tipo que puede ser una casilla y solo puede ser B, C, S, E.';
+ COMMENT ON COLUMN casillas.aprobada IS 'Hace referencia a si la casilla esta aprobada o no, teniendo como dominio S o N.';
+--casillas
+
+--Restricciones y comentarios de Partidos_Politicos
+--partidos
+ALTER TABLE partidos ADD CONSTRAINT
+  PKPartidos PRIMARY KEY (id_estado, id_distrito_federal, id_partido);
+ALTER TABLE partidos ADD CONSTRAINT
+  FKEstadosP FOREIGN KEY (id_estado)
+             REFERENCES estados (id_estado) match full;
+ALTER TABLE partidos ADD CONSTRAINT
+  FKDistritosFederales FOREIGN KEY (id_estado, id_distrito_federal)
+                       REFERENCES distritos_federales (id_estado, id_distrito_federal) match full;
+COMMENT ON TABLE partidos IS 'Tabla con todos los partidos politicos.';
+  COMMENT ON COLUMN partidos.id_estado IS 'Identificador del estado al cual pertenece el partido politico.';
+  COMMENT ON COLUMN partidos.id_distrito_federal IS 'Identificador del distrito federal al cual pertenece el partido politico.';
+  COMMENT ON COLUMN partidos.id_partido IS 'Identificador del partido politico.';
+  COMMENT ON COLUMN partidos.nombre IS 'Nombre del partido politico.';
+  COMMENT ON COLUMN partidos.siglas IS 'Siglas del partido politico.';
+--particos
+
+--Esquema de Representantes
+--representantes_preliminares
+CREATE TABLE representantes.representantes_preliminares(
+  id_representante Integer PRIMARY KEY,
+  id_partido Integer,
+  nombre varchar(50),
+  apellido_paterno varchar(50),
+  apellido_materno varchar(50),
+  tipo_representante char(1) check (tipo_representante in ('G','C'))
+    CONSTRAINT FKPartidoRP FOREIGN KEY (id_partido)
+                           REFERENCES partidos (id_partido)
+
+);
+--representantes_preliminares
+
+--representantes_generales
+--representantes_generales
+
+--representantes_ante_casilla
+--representantes_ante_casilla
+
+--representantes_aprobados
+CREATE TABLE representantes.representantes_aprobados(
+  id_representante Integer REFERENCES representantes_preliminares (id_representante),
+
+);
+--representantes_aprobados
